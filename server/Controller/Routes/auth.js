ea6cd1033ee947707280d394e6d17e2afea0e24d
Michael Walter <michael_patrick.walter@outlook.com>
2021-09-24 22:47:32 -0700

    Server: externalized login functionality into its own helper, tested and working perfectly.

--- server/Controller/Routes/auth.js
+++ server/Controller/Routes/auth.js
@@ -1,6 +1,4 @@
 const authRouter = require('express').Router();
-const loginModel = require('../../Model/loginModel');
-const userModel = require('../../Model/userModel');
 var bcrypt = require('bcryptjs');
 var salt = bcrypt.genSaltSync(14);
 const mongoose = require('mongoose');
@@ -10,6 +8,7 @@ const session = require('express-session');
 // authRouter-Helpers
 const register = require('../../Helpers/authHelpers/register');
 const updateCredentials = require('../../Helpers/authHelpers/updateCredentials');
+const login = require('../../Helpers/authHelpers/login');
 
 // Register User
 authRouter.post('/register', (req,res) => {
@@ -24,45 +23,16 @@ authRouter.post('/login', (req,res) => {
     if (!name | !password) {
         return res.status(400).send('Error')
     }
-    loginModel.findOne({name})
-    .then(user => {
-        const auth = bcrypt.compareSync(password, user.password)
-        console.log(auth);
-        if (auth) {
-            return res.status(200).send('Success')
-        }
-        else {
-            return res.status(400).send('Error')
-        }
-    })
-    .catch((err) => {
-        return res.status(400).send('Error')
-    })
+    else {
+        login(name,password,bcrypt,res)
+    }
 })
 
-// Update User Credentials
+// Update Credentials
 authRouter.put('/update', (req,res) => {
     const {_id,name, password} = req.body;
     const hash = bcrypt.hashSync(password,salt)
     updateCredentials(_id,name,hash,res);
 })
-
-
-// Update User
-// Login User
-authRouter
-    .post('/', (req,res) => {
-        console.log(req.body)
-        loginModel.findOne(req.body)
-        .then((user) => {
-            console.log(user)
-            if(!user) return res.status(400).send('Incorrect email or password')
-            res.cookie('user', user)
-            res.send(true)
-        })
-        .catch(err => {
-            if (err) res.status(400).send(err)
-        })
-    })
 
 module.exports = authRouter;
